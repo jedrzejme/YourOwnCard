b79fa791cb20c4554d3fbfdfed59a0ea3ff05db3
Jedrzej.me <50214379+jedrzejme@users.noreply.github.com>
2024-08-18 16:09:14 +0200

    Repair problem with password [1.8.3]
    Repair problem with usage of password with special characters in it

--- app.py
+++ app.py
@@ -55,9 +55,12 @@ def hash_password(password):
     hashed_password = hashlib.sha256(password.encode()).hexdigest()
     return hashed_password
 
-def create_profile(email, username, password, display_name, background_color, container_color, button_color, button_hover_color, button_text_color, text_color, footer_color, about_me, www, www_name, instagram, instagram_name, facebook, facebook_name, x, x_name, github, github_name, linkedin, linkedin_name, discord, steam, steam_name, profile_picture_path):
+def create_profile(editing, email, username, password, display_name, background_color, container_color, button_color, button_hover_color, button_text_color, text_color, footer_color, about_me, www, www_name, instagram, instagram_name, facebook, facebook_name, x, x_name, github, github_name, linkedin, linkedin_name, discord, steam, steam_name, profile_picture_path):
     password = password
-    hashed_password = hash_password(password)
+    if not editing:
+        hashed_password = hash_password(password)
+    else:
+        hashed_password = password
     background_color = background_color or default_background_color
     container_color = container_color or default_container_color
     button_color = button_color or default_button_color
@@ -76,7 +79,7 @@ def create_profile(email, username, password, display_name, background_color, co
     profile_file.write(f"""email = {email}\nusername = {username}\nhashed_password = {hashed_password}\ndisplay_name = {display_name}\nbackground_color = {background_color}\ncontainer_color = {container_color}\nbutton_color = {button_color}\nbutton_hover_color = {button_hover_color}\nbutton_text_color = {button_text_color}\ntext_color = {text_color}\nfooter_color = {footer_color}\nabout_me = {about_me}\nwww = {www}\nwww_name = {www_name}\ninstagram_name = {instagram_name}\ninstagram = {instagram}\nfacebook_name = {facebook_name}\nfacebook = {facebook}\nx_name = {x_name}\nx = {x}\ngithub_name = {github_name}\ngithub = {github}\nlinkedin_name = {linkedin_name}\nlinkedin = {linkedin}\ndiscord = {discord}\nsteam_name = {steam_name}\nsteam = {steam}\nprofile_picture_path = {profile_picture_path}""")
     profile_file.close()
 
-def create_profile_with_picture(email, username, password, display_name, profile_picture, background_color, container_color, button_color, button_hover_color, button_text_color, text_color, footer_color, about_me, www, www_name, instagram, instagram_name, facebook, facebook_name, x, x_name, github, github_name, linkedin, linkedin_name, discord, steam, steam_name):
+def create_profile_with_picture(editing, email, username, password, display_name, profile_picture, background_color, container_color, button_color, button_hover_color, button_text_color, text_color, footer_color, about_me, www, www_name, instagram, instagram_name, facebook, facebook_name, x, x_name, github, github_name, linkedin, linkedin_name, discord, steam, steam_name):
     if request.method == "POST":
         display_name = request.form.get("display-name")
         background_color = request.form.get("background-color")
@@ -125,7 +128,7 @@ def create_profile_with_picture(email, username, password, display_name, profile
         else:
             profile_picture_path = load_profile(f"protected/profiles/{username}.txt")["profile_picture_path"]
 
-        create_profile(email, username, password, display_name, background_color, container_color, button_color, button_hover_color, button_text_color, text_color, footer_color, about_me, www, www_name, instagram, instagram_name, facebook, facebook_name, x, x_name, github, github_name, linkedin, linkedin_name, discord, steam, steam_name, profile_picture_path)
+        create_profile(editing, email, username, password, display_name, background_color, container_color, button_color, button_hover_color, button_text_color, text_color, footer_color, about_me, www, www_name, instagram, instagram_name, facebook, facebook_name, x, x_name, github, github_name, linkedin, linkedin_name, discord, steam, steam_name, profile_picture_path)
         
         return redirect(f"/profile/{username}")
     
@@ -155,10 +158,11 @@ def submit_creation_of_profile():
     if enable_creation_of_new_profiles:
         username = request.form.get("username")
         if not os.path.exists(f"protected/profiles/{username}.txt"):
+            editing = False
             profile_picture = request.files['profile-picture']
             email = request.form.get("email")
             username = request.form.get("username")
-            password = request.form.get("password")
+            password = request.form.get("""password""")
             display_name = request.form.get("display-name")
             background_color = request.form.get("background-color")
             container_color = request.form.get("container-color")
@@ -183,7 +187,7 @@ def submit_creation_of_profile():
             x_name = request.form.get("x-name")
             github = request.form.get("github")
             github_name = request.form.get("github-name")
-            create_profile_with_picture(email=email, username=username, password=password, display_name=display_name, profile_picture=profile_picture, background_color=background_color, container_color=container_color, button_color=button_color, button_hover_color=button_hover_color, button_text_color=button_text_color, text_color=text_color, footer_color=footer_color, about_me=about_me, www=www, www_name=www_name, instagram=instagram, instagram_name=instagram_name, facebook=facebook, facebook_name=facebook_name, x=x, x_name=x_name, github=github, github_name=github_name, linkedin=linkedin, linkedin_name=linkedin_name, discord=discord, steam=steam, steam_name=steam_name)
+            create_profile_with_picture(editing = editing, email=email, username=username, password=password, display_name=display_name, profile_picture=profile_picture, background_color=background_color, container_color=container_color, button_color=button_color, button_hover_color=button_hover_color, button_text_color=button_text_color, text_color=text_color, footer_color=footer_color, about_me=about_me, www=www, www_name=www_name, instagram=instagram, instagram_name=instagram_name, facebook=facebook, facebook_name=facebook_name, x=x, x_name=x_name, github=github, github_name=github_name, linkedin=linkedin, linkedin_name=linkedin_name, discord=discord, steam=steam, steam_name=steam_name)
         else:
             return redirect("/create-profile")
         return redirect(f"/profile/{username}")
@@ -196,17 +200,18 @@ def edit_profile_auth():
 @app.route('/edit-profile', methods=['GET', 'POST'])
 def edit_profile():
     username = request.form.get("username")
-    password = request.form.get("password")
+    password = request.form.get("""password""")
     profile = load_profile(f"protected/profiles/{username}.txt")
     hashed_password = hash_password(password)
-    if profile["hashed_password"] == hashed_password:
-        return render_template('edit-profile.html', password = password, default_background_color=default_background_color, default_container_color=default_container_color, default_button_color=default_button_color, default_button_hover_color = default_button_hover_color, default_button_text_color = default_button_text_color, default_input_color = default_input_color, default_text_color = default_text_color, default_footer_color = default_footer_color, email=profile["email"], username=username, display_name=profile["display_name"], background_color=profile["background_color"], container_color=profile["container_color"], button_color=profile["button_color"], button_hover_color=profile["button_hover_color"], button_text_color=profile["button_text_color"], text_color=profile["text_color"], footer_color=profile["footer_color"], about_me=profile["about_me"], www=profile["www"], www_name=profile["www_name"], instagram=profile["instagram"], instagram_name=profile["instagram_name"], facebook=profile["facebook"], facebook_name=profile["facebook_name"], x=profile["x"], x_name=profile["x_name"], github=profile["github"], github_name=profile["github_name"], linkedin=profile["linkedin"], linkedin_name=profile["linkedin_name"], discord=profile["discord"], steam=profile["steam"], steam_name=profile["steam_name"])
+    if profile["""hashed_password"""] == hashed_password:
+        return render_template('edit-profile.html', password = hashed_password, default_background_color=default_background_color, default_container_color=default_container_color, default_button_color=default_button_color, default_button_hover_color = default_button_hover_color, default_button_text_color = default_button_text_color, default_input_color = default_input_color, default_text_color = default_text_color, default_footer_color = default_footer_color, email=profile["email"], username=username, display_name=profile["display_name"], background_color=profile["background_color"], container_color=profile["container_color"], button_color=profile["button_color"], button_hover_color=profile["button_hover_color"], button_text_color=profile["button_text_color"], text_color=profile["text_color"], footer_color=profile["footer_color"], about_me=profile["about_me"], www=profile["www"], www_name=profile["www_name"], instagram=profile["instagram"], instagram_name=profile["instagram_name"], facebook=profile["facebook"], facebook_name=profile["facebook_name"], x=profile["x"], x_name=profile["x_name"], github=profile["github"], github_name=profile["github_name"], linkedin=profile["linkedin"], linkedin_name=profile["linkedin_name"], discord=profile["discord"], steam=profile["steam"], steam_name=profile["steam_name"])
     else:
         return redirect("/edit-profile-auth")
 
-@app.route('/submit-edition-of-profile/<username>/<password>', methods=['GET', 'POST'])
+@app.route("""/submit-edition-of-profile/<username>/<password>""", methods=['GET', 'POST'])
 def submit_edition_of_profile(username, password):
     if enable_editing_after_creation:
+        editing = True
         profile_picture = request.files['profile-picture']
         email = load_profile(f"protected/profiles/{username}.txt")["email"]
         display_name = request.form.get("display-name")
@@ -233,7 +238,7 @@ def submit_edition_of_profile(username, password):
         x_name = request.form.get("x-name")
         github = request.form.get("github")
         github_name = request.form.get("github-name")
-        create_profile_with_picture(email=email, username=username, password=password, display_name=display_name, profile_picture=profile_picture, background_color=background_color, container_color=container_color, button_color=button_color, button_hover_color=button_hover_color, button_text_color=button_text_color, text_color=text_color, footer_color=footer_color, about_me=about_me, www=www, www_name=www_name, instagram=instagram, instagram_name=instagram_name, facebook=facebook, facebook_name=facebook_name, x=x, x_name=x_name, github=github, github_name=github_name, linkedin=linkedin, linkedin_name=linkedin_name, discord=discord, steam=steam, steam_name=steam_name)
+        create_profile_with_picture(editing = editing, email=email, username=username, password=password, display_name=display_name, profile_picture=profile_picture, background_color=background_color, container_color=container_color, button_color=button_color, button_hover_color=button_hover_color, button_text_color=button_text_color, text_color=text_color, footer_color=footer_color, about_me=about_me, www=www, www_name=www_name, instagram=instagram, instagram_name=instagram_name, facebook=facebook, facebook_name=facebook_name, x=x, x_name=x_name, github=github, github_name=github_name, linkedin=linkedin, linkedin_name=linkedin_name, discord=discord, steam=steam, steam_name=steam_name)
         return redirect(f"/profile/{username}")
 
 @app.route('/profile/<username>')
